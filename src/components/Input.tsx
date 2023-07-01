384a3e5fa7a0b60b8655d028ca5500ef5e9cea1f
Dohyun Kim <kim@dohyun.kr>
2023-07-02 03:33:26 +0900

    fix: input classname

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -48,7 +48,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           'focus:outline-none focus-visible:ring-0',
           'focus:border-schoolmate-300 focus:ring-schoolmate-300"',
           'shadow-sm',
-          'px-3 transition-colors duration-75'
+          'disabled:bg-[#EEEEEE]',
+          'px-3 transition-colors duration-75',
+          className
         )}
         {...rest}
       >
