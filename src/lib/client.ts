22cf5bba6b05a77f52cbbfa11718ba31dbafdf11
Dohyun Kim <kim@dohyun.kr>
2023-07-01 23:02:12 +0900

    fix(lib): SWR Fetcher

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -12,10 +12,24 @@ client.interceptors.response.use(
   (response) => response,
   (error: AxiosError) => {
     if (error.response?.status === 401) {
-      window.location.href = '/login';
+      window.location.href = '/auth/login';
     }
     return Promise.reject(error);
   }
 );
+
+export const swrfetcher = async <JSON = unknown>(
+  endpoints: string
+): Promise<JSON> =>
+  client
+    .get(endpoints, {
+      withCredentials: true,
+    })
+    .then((data) => data.data.data)
+    .catch((e: AxiosError) => {
+      const response = e.response as unknown as { data: { message: string } };
+
+      throw new Error(response?.data?.message || 'Something went wrong');
+    });
 
 export default client;
