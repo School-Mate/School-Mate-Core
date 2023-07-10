59e0fee419df29200cc804c05778b0b552853511
Dohyun Kim <kim@dohyun.kr>
2023-07-10 17:27:23 +0900

    fix: Dashboard Left Section Types

--- src/components/Dashboard/LeftSection.tsx
+++ src/components/Dashboard/LeftSection.tsx
@@ -7,18 +7,29 @@ import clsxm from '@/lib/clsxm';
 
 import Advertisement from '@/components/Advertisement';
 import BoardItemButton from '@/components/BoardItem';
-import Asked from '@/components/Dashboard/Asked';
+import WigetAsked from '@/components/Dashboard/Asked';
+import WigetReview from '@/components/Dashboard/Review';
+import Loading from '@/components/Loading';
+
+import { Article } from '@/types/article';
+import { AskedUser } from '@/types/asked';
+import { Board } from '@/types/board';
+import { Review } from '@/types/review';
 
 interface DashboardLeftSectionProps {
-  articles: any[];
-  askeds: any[];
-  boards: any[];
+  articles: Article[];
+  askeds?: AskedUser[];
+  boards: Board[];
+  reviews: Review[];
+  isVerifySchool: boolean;
 }
 
 const DashboardLeftSection: NextPage<DashboardLeftSectionProps> = ({
   articles,
   askeds,
   boards,
+  reviews,
+  isVerifySchool,
 }) => {
   const [askSwiper, setAskSwiper] = React.useState<SwiperClass>();
   const [askedIndex, setAskedIndex] = React.useState<number>(0);
@@ -61,16 +72,7 @@ const DashboardLeftSection: NextPage<DashboardLeftSectionProps> = ({
           <div className='mt-4 flex flex-row'>
             <div className='mr-8 flex h-[280px] w-full max-w-[474px] flex-col justify-between'>
               {articles.slice(0, 3).map((article, index) => (
-                <BoardItemButton
-                  key={index}
-                  title={article.title}
-                  content={article.content}
-                  createdAt={article.createdAt}
-                  isAnonymous={article.isAnonymous}
-                  user={article.user}
-                  board={article.board}
-                  titleImage={article.titleImage}
-                />
+                <BoardItemButton key={index} article={article} />
               ))}
             </div>
             <div className='grid w-[318px] grid-cols-2 rounded-[10px] border py-6'>
@@ -106,64 +108,79 @@ const DashboardLeftSection: NextPage<DashboardLeftSectionProps> = ({
           <Advertisement />
         </div>
         <div className='relative flex w-full max-w-[874px] flex-row justify-between rounded-[20px] border-2 border-[#E3E5E8] p-5'>
-          <button
-            className={clsxm(
-              'text-xl font-bold',
-              'hover:text-[#BEBEBE]',
-              'bottom-[50%] top-[50%] flex -translate-y-1/2 transform flex-row items-center bg-white',
-              'absolute left-1 z-10 flex h-9 w-9 rounded-full border border-[#BEBEBE]',
-              askedIndex === 0 ? 'hidden' : 'block'
-            )}
-            onClick={() => {
-              askSwiper?.slidePrev();
-            }}
-          >
-            <i className='fas fa-chevron-left mx-auto text-lg' />
-          </button>
-          <button
-            className={clsxm(
-              'text-xl font-bold',
-              'hover:text-[#BEBEBE]',
-              'bottom-[50%] top-[50%] flex -translate-y-1/2 transform flex-row items-center bg-white',
-              'absolute right-1 z-10 flex h-9 w-9 rounded-full border border-[#BEBEBE]'
-            )}
-            onClick={() => {
-              askSwiper?.slideNext();
-            }}
-          >
-            <i className='fas fa-chevron-right mx-auto text-lg' />
-          </button>
-          <Swiper
-            slidesPerView={3}
-            centeredSlides={false}
-            slidesPerGroupSkip={3}
-            grabCursor={true}
-            keyboard={{
-              enabled: true,
-            }}
-            navigation={false}
-            pagination={{
-              clickable: true,
-            }}
-            onSwiper={setAskSwiper}
-            onSlideChange={(swiper) => {
-              setAskedIndex(swiper.activeIndex);
-            }}
-            spaceBetween={20}
-            loop={true}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: true,
-            }}
-          >
-            {askeds.map((asked, index) => (
-              <>
-                <SwiperSlide key={index}>
-                  <Asked user={asked.user} title={asked.title} key={index} />
-                </SwiperSlide>
-              </>
-            ))}
-          </Swiper>
+          {askeds ? (
+            <>
+              <button
+                className={clsxm(
+                  'text-xl font-bold',
+                  'hover:text-[#BEBEBE]',
+                  'bottom-[50%] top-[50%] flex -translate-y-1/2 transform flex-row items-center bg-white',
+                  'absolute left-1 z-10 flex h-9 w-9 rounded-full border border-[#BEBEBE]',
+                  askedIndex === 0 ? 'hidden' : 'block'
+                )}
+                onClick={() => {
+                  askSwiper?.slidePrev();
+                }}
+              >
+                <i className='fas fa-chevron-left mx-auto text-lg' />
+              </button>
+              <button
+                className={clsxm(
+                  'text-xl font-bold',
+                  'hover:text-[#BEBEBE]',
+                  'bottom-[50%] top-[50%] flex -translate-y-1/2 transform flex-row items-center bg-white',
+                  'absolute right-1 z-10 flex h-9 w-9 rounded-full border border-[#BEBEBE]'
+                )}
+                onClick={() => {
+                  askSwiper?.slideNext();
+                }}
+              >
+                <i className='fas fa-chevron-right mx-auto text-lg' />
+              </button>
+              <Swiper
+                slidesPerView={3}
+                centeredSlides={false}
+                slidesPerGroupSkip={3}
+                grabCursor={true}
+                keyboard={{
+                  enabled: true,
+                }}
+                navigation={false}
+                pagination={{
+                  clickable: true,
+                }}
+                onSwiper={setAskSwiper}
+                onSlideChange={(swiper) => {
+                  setAskedIndex(swiper.activeIndex);
+                }}
+                spaceBetween={20}
+                loop={true}
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: true,
+                }}
+              >
+                {askeds.map((asked, index) => (
+                  <>
+                    <SwiperSlide key={index}>
+                      <WigetAsked askedUser={asked} key={index} />
+                    </SwiperSlide>
+                  </>
+                ))}
+              </Swiper>
+            </>
+          ) : (
+            <>
+              <div className='flex h-[134px] w-full flex-col items-center justify-center'>
+                <Loading />
+              </div>
+            </>
+          )}
+        </div>
+        <div className='relative flex w-full max-w-[874px] flex-row justify-between rounded-[20px] border-2 border-[#E3E5E8] p-5'>
+          {reviews.map((review, index) => (
+            <WigetReview review={review} key={index} />
+          ))}
         </div>
       </div>
     </>
