d7561e438ab58e0d3683c3fa0aec0444b23fd903
Dohyun Kim <kim@dohyun.kr>
2023-07-10 17:27:00 +0900

    fix: bus wiget

--- src/components/Dashboard/Bus.tsx
+++ src/components/Dashboard/Bus.tsx
@@ -1,8 +1,8 @@
-import { ISchoolInfoRow } from '@/types/school';
+import { School } from '@/types/school';
 import { User } from '@/types/user';
 
 interface WigetBusProps {
-  school: ISchoolInfoRow;
+  school: School;
   user: User;
 }
 
@@ -10,7 +10,9 @@ const WigetBus: React.FC<WigetBusProps> = ({ school }) => {
   return (
     <div className='flex h-[172px] w-full flex-col items-center justify-center rounded-[10px] border bg-white'>
       <h2 className='text-sm font-semibold'>[버스 도착정보]</h2>
-      <div className='mt-1 flex h-[100px] flex-col items-center justify-center px-5 text-center text-sm'></div>
+      <div className='mt-1 flex h-[100px] flex-col items-center justify-center px-5 text-center text-sm'>
+        개발중
+      </div>
     </div>
   );
 };
