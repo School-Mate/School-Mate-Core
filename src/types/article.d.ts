97d47f74968b39fbc77dae4dcf16284442d0b79a
Dohyun Kim <kim@dohyun.kr>
2023-07-13 21:35:42 +0900

    feat: article detail

--- src/types/article.d.ts
+++ src/types/article.d.ts
@@ -1,3 +1,48 @@
+import { User } from './user';
+
 export interface Article {
-  [keyof: string]: any;
+  id: number;
+  schoolId: string;
+  title: string;
+  content: string;
+  images: string[];
+  isAnonymous: boolean;
+  userId: string;
+  createdAt: Date;
+  boardId: number;
+  Board: Board;
+  User: User;
+  keyOfImages: string[];
+}
+
+export interface Board {
+  id: number;
+  schoolId: string;
+  name: string;
+  description: string;
+  noticeId: number | null;
+}
+
+export interface Comment {
+  User?: User;
+  articleId: number;
+  content: string;
+  createdAt: Date;
+  id: number;
+  isAnonymous: boolean;
+  recomments: Recomment[];
+  updatedAt: Date;
+  userId: string;
+}
+
+export interface Recomment {
+  User?: User;
+  articleId: number;
+  commentId: number;
+  content: string;
+  createdAt: Date;
+  id: number;
+  isAnonymous: boolean;
+  updatedAt: Date;
+  userId: string;
 }
