6b8dec469c618fe7d60208599b6c1e50951be09c
Dohyun Kim <kim@dohyun.kr>
2023-07-13 21:30:23 +0900

    fix: ads

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -26,6 +26,34 @@ const Advertisement: React.FC<AdvertisementProps> = ({ size = 'short' }) => {
   );
 };
 
+export const AdvertisementSqaure: React.FC<AdvertisementProps> = ({
+  size = 'short',
+}) => {
+  return (
+    <div
+      className={`z-0 mx-auto w-full text-center text-white ${
+        process.env.NODE_ENV === 'production' ? '' : 'bg-black py-12'
+      }`}
+      style={size === 'short' ? { width: '90px' } : { width: '330px' }}
+    >
+      {process.env.NODE_ENV === 'production' ? (
+        <AdSense.Google
+          style={{
+            display: 'inline-block',
+            width: size === 'short' ? '90px' : '330px',
+            height: '100%',
+          }}
+          client='ca-pub-2701426579223876'
+          slot='7698754986'
+          format=''
+        />
+      ) : (
+        'Ads'
+      )}
+    </div>
+  );
+};
+
 declare global {
   interface Window {
     adsbygoogle: {
