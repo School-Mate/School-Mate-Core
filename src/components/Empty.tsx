2e25501b885813e3de8c963b9eb6e129c4d0bbfe
Dohyun Kim <kim@dohyun.kr>
2023-07-17 00:25:33 +0900

    fix(componets): Empty Customize size

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,15 +1,24 @@
+import clsxm from '@/lib/clsxm';
+
 import LottieAnimaition from '@/components/LottieAnimaition';
 
-const Empty = () => {
+interface EmptyProps {
+  className?: string;
+  textClassName?: string;
+}
+
+const Empty: React.FC<EmptyProps> = ({ className, textClassName }) => {
   return (
     <>
-      <div className='h-full w-full'>
+      <div className={clsxm('h-full w-full')}>
         <div className='flex h-full flex-col items-center justify-center'>
           <LottieAnimaition
-            className='-mb-20 -mt-20 h-96 w-96'
+            className={clsxm('-mb-20 -mt-20 h-96 w-96', className)}
             animation={require('@/lottieFiles/empty.json')}
           />
-          <h1 className='text-center text-2xl font-bold'>
+          <h1
+            className={clsxm('text-center text-2xl font-bold', textClassName)}
+          >
             오..이런! 아무것도 없네요ㅠ.ㅠ
           </h1>
         </div>
