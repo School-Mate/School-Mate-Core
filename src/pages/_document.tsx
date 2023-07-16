e4640f392d6cf00e222b4558b95b86b50c59fe9a
Dohyun Kim <kim@dohyun.kr>
2023-07-17 00:36:04 +0900

    feat(Ads): adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,6 +30,11 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap'
           rel='stylesheet'
         />
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2701426579223876'
+          crossOrigin='anonymous'
+        ></script>
       </Head>
       <body>
         <Main />
